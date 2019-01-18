e6034c163acd8f47c9ca76058c4d32f630d0c555
sada <sada913@gmail.com>
2019-01-18 16:57:40 +0900

    Add Project

---
+++ Assets/Script/ParticlePainter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Es.InkPainter;
+using UnityEngine;
+
+namespace Onoty3D
+{
+    public class ParticlePainter : MonoBehaviour
+    {
+        public Brush Brush = null;
+        public ParticleSystem Particle;
+
+        private List<ParticleCollisionEvent> _collisionEvents;
+
+        // Use this for initialization
+        private void Start()
+        {
+            this.Particle = this.GetComponent<ParticleSystem>();
+            this._collisionEvents = new List<ParticleCollisionEvent>(16);
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+
+        private void OnParticleCollision(GameObject other)
+        {
+            var safeSize = Particle.GetSafeCollisionEventSize();
+            if (this._collisionEvents.Count < safeSize)
+            {
+                this._collisionEvents = new List<ParticleCollisionEvent>(safeSize);
+            }
+
+            this.Particle.GetCollisionEvents(other, this._collisionEvents);
+
+            foreach (var collisionEvent in this._collisionEvents)
+            {
+                if (collisionEvent.colliderComponent == null)
+                {
+                    continue;
+                }
+
+                var canvas = collisionEvent.colliderComponent.GetComponent<InkCanvas>();
+                if (canvas == null)
+                {
+                    continue;
+                }
+                canvas.PaintNearestTriangleSurface(Brush, collisionEvent.intersection);
+            }
+        }
+    }
+}
